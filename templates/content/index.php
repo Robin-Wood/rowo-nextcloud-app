779c52d0cecb4ec675e5b0656c977894ce8cc765
Tim Christensen <mail@timchristensen.de>
2018-07-21 22:25:58 +0200

    fix

--- templates/content/index.php
+++ templates/content/index.php
@@ -1,32 +1,11 @@
 <div class="section screen">
 
-  <div class="screen1">
-    <div class="imageBox">
-      <div id="dropZone">
-        <div>
-          <p>Dieses Werkzeug wandelt Bilder in schwarz-grüne Bilder für ROBIN WOOD-Veröffentlichungen um.</p>
-          <p>Bild hier hereinziehen oder unten hochladen<span class="fontpreload">.</span></p>
-        </div>
-      </div>
-      <div class="controls">
-        <input type="file" name="file" id="file" accept="image/*" capture>
-      </div>
+  <div class="imageBox">
+    <canvas id="twitterImage" class="finalImage"></canvas>
+    <div class="controls">
+      <button id="downloadIt" class="primary">Herunterladen</button>
+      <button id="startOver" class="">Anderes Bild hochladen</button>
     </div>
   </div>
 
-  <div class="screen2">
-    <div class="imageBox">
-	     <canvas id="image" class="finalImage"></canvas>
-       <div class="controls">
-         <button id="downloadIt" class="primary">Herunterladen</button>
-         <button id="startOver" class="">Anderes Bild hochladen</button>
-       </div>
-    </div>
-  </div>
-
-  <div class="outputs">
-    <div class="imageBox">
-      <canvas id="twitterImage" class="finalImage"></canvas>
-    </div>
-  </div>
 </div>
