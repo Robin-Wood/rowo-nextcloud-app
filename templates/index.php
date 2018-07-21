82e3bac202db51d4321905cff7041411b2d7102a
Tim Christensen <mail@timchristensen.de>
2018-07-21 21:59:03 +0200

    fix

--- templates/index.php
+++ templates/index.php
@@ -5,10 +5,15 @@ style('robinwoodapp', 'style');
 ?>
 
 <div id="app">
+	<div id="app-navigation">
+		<label>
+			<span>Slogan</span>
+			<input type="text" id="slogan" />
+		</label>
+	</div>
 	<div id="app-content">
 		<div id="app-content-wrapper">
 			<?php print_unescaped($this->inc('content/index')); ?>
 		</div>
 	</div>
 </div>
-
