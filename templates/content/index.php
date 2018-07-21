9f348c309cd9efcec727136178920d9492f1a270
Tim Christensen <mail@timchristensen.de>
2018-07-21 22:21:45 +0200

    fix

--- templates/content/index.php
+++ templates/content/index.php
@@ -1,9 +1,22 @@
 <div class="section">
-  <div class="imageBox">
-    <canvas id="twitterImage" class="finalImage"></canvas>
-    <div class="controls">
-      <span>Twitter</span>
-      <button id="downloadIt" class="primary">Herunterladen</button>
+
+
+    <div class="imageBox">
+	     <canvas id="image" class="finalImage"></canvas>
+       <div class="controls">
+         <button id="downloadIt" class="primary">Herunterladen</button>
+         <button id="startOver" class="">Anderes Bild hochladen</button>
+       </div>
+    </div>
+  </div>
+
+  <div class="outputs">
+    <div class="imageBox">
+      <canvas id="twitterImage" class="finalImage"></canvas>
+      <div class="controls">
+        <button id="downloadIt" class="primary">Herunterladen</button>
+        <button id="startOver" class="">Anderes Bild hochladen</button>
+      </div>
     </div>
   </div>
 </div>
