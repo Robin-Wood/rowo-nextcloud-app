36f8d457c7b861b14771c60ec4c13c3df5d603aa
Tim Christensen <mail@timchristensen.de>
2018-07-21 23:03:32 +0200

    fix

--- templates/content/index.php
+++ templates/content/index.php
@@ -1,16 +1,20 @@
 <div class="section screen">
 
   <div class="imageBox">
-    <canvas id="vanillaImage" class="finalImage"></canvas>
-    <div class="controls">
+    <div class="card-up"
+      <canvas id="vanillaImage" class="finalImage"></canvas>
+    </div>
+    <div class="card-down">
       <span>Bild</span>
       <button id="downloadItVanilla" class="primary">Herunterladen</button>
     </div>
   </div>
 
   <div class="imageBox">
-    <canvas id="twitterImage" class="finalImage"></canvas>
-    <div class="controls">
+    <div class="card-up">
+      <canvas id="twitterImage" class="finalImage"></canvas>
+    </div>
+    <div class="card-down">
       <span>Twitter: Slogan</span>
       <button id="downloadItTwitter" class="primary">Herunterladen</button>
     </div>
