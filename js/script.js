d335380d78d0a76c556d5499d2fac5b52c6fe608
Tim Christensen <mail@timchristensen.de>
2018-07-23 14:04:09 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -84,33 +84,45 @@ function renderImageTwitter(ctx){
     if(slogan.length > 0) {
       ctx.textAlign = "center";
       ctx.font = '130px Plakkaat';
-      if (ctx.measureText(slogan).width < twitterWidth*0.85) {
+      if (ctx.measureText(slogan).width < twitterWidth*margin) {
         ctx.fillText(slogan, twitterWidth/2, twitterHeight/2);
       } else {
         ctx.font = '100px Plakkaat';
-        if (ctx.measureText(slogan).width < twitterWidth*0.85) {
+        if (ctx.measureText(slogan).width < twitterWidth*margin) {
           ctx.fillText(slogan, twitterWidth/2, twitterHeight/2);
         } else {
           var words = slogan.split(" ");
           let slogan1 = words.shift();
-          while(ctx.measureText(slogan1).width < ctx.measureText(slogan).width/2 && words.length > 1 && ctx.measureText(slogan1 + " " + words[0]).width < twitterWidth*0.85) {
+          while(ctx.measureText(slogan1).width < ctx.measureText(slogan).width/2 && words.length > 1 && ctx.measureText(slogan1 + " " + words[0]).width < twitterWidth*margin) {
             slogan1 += " " + words.shift();
           }
           let slogan2 = words.join(" ");
-          if(ctx.measureText(slogan2).width < twitterWidth*0.85) {
+          if(ctx.measureText(slogan2).width < twitterWidth*margin) {
             ctx.fillText(slogan1, twitterWidth/2, twitterHeight/2-100/2*lineHeight);
             ctx.fillText(slogan2, twitterWidth/2, twitterHeight/2+100/2*lineHeight);
           } else {
             ctx.font = '80px Plakkaat';
             var words = slogan.split(" ");
             let slogan1 = words.shift();
-            while(ctx.measureText(slogan1).width < ctx.measureText(slogan).width/2 && words.length > 1 && ctx.measureText(slogan1 + " " + words[0]).width < twitterWidth*0.85) {
+            while(ctx.measureText(slogan1).width < ctx.measureText(slogan).width/2 && words.length > 1 && ctx.measureText(slogan1 + " " + words[0]).width < twitterWidth*margin) {
               slogan1 += " " + words.shift();
             }
             let slogan2 = words.join(" ");
-            if(ctx.measureText(slogan2).width < twitterWidth*0.85) {
+            if(ctx.measureText(slogan2).width < twitterWidth*margin) {
               ctx.fillText(slogan1, twitterWidth/2, twitterHeight/2-80/2*lineHeight);
               ctx.fillText(slogan2, twitterWidth/2, twitterHeight/2+80/2*lineHeight);
+            } else {
+              var words = slogan2.split(" ");
+              let slogan21 = words.shift();
+              while(ctx.measureText(slogan21).width < ctx.measureText(slogan2).width/2 && words.length > 1 && ctx.measureText(slogan21 + " " + words[0]).width < twitterWidth*margin) {
+                slogan21 += " " + words.shift();
+              }
+              let slogan21 = words.join(" ");
+              if(ctx.measureText(slogan22).width < twitterWidth*margin) {
+                ctx.fillText(slogan1, twitterWidth/2, twitterHeight/2-10-80*lineHeight);
+                ctx.fillText(slogan21, twitterWidth/2, twitterHeight/2-10+80/2*lineHeight);
+                ctx.fillText(slogan22, twitterWidth/2, twitterHeight/2-10+80*lineHeight);
+              }
             }
           }
         }
