24038a8ca525496e559aaabfff512aad508cde07
Tim Christensen <mail@timchristensen.de>
2018-09-05 15:59:15 +0200

    refactor

--- js/script.js
+++ js/script.js
@@ -392,6 +392,6 @@ function getFlexbox(id, text) {
   node.setAttribute('class', 'flexBox');
   node.setAttribute('id', id);
   node.innerHTML = '<div class="imageBox"><div class="card-up"><canvas class="finalImage"></canvas></div><div class="card-down"><span>' + text + '</span><button class="primary download">Herunterladen</button></div></div>'
-  var wrapper = document.document.getElementsByClassName('screen');
+  var wrapper = document.getElementsByClassName('screen');
   wrapper.appendChild(node);
 }
