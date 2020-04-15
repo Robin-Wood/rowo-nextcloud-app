4cffd13f40592426e96d07e26e2f143d34d2a693
Tim Christensen <tim.christensen@robinwood.de>
2020-04-15 10:07:26 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -99,8 +99,8 @@ function renderImageLogo(id, name, width, height){
         ctx.globalAlpha=1;
         ctx.fillStyle = 'white';
         ctx.textAlign = "left";
-        ctx.font = width*height*0.000016 + 'px MarkOT';
-        ctx.fillText('Foto: ' + quelle, width*height*0.000016, height-width*height*0.000016);
+        ctx.font = (width+height)*0.0092 + 'px MarkOT';
+        ctx.fillText('Foto: ' + quelle, (width+height)*0.0092, height-(width+height)*0.0092);
       }
       if(currentId == id) {
         updateDetail(currentId);
